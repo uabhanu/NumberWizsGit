325ed243914caf088e05f4b898093096e1b12ad1
Bhanu <uabhanu@gmail.com>
2017-07-29 10:21:17 +0100

    Renamed Game to NumbersWizard

---
+++ NumbersWizard/Assets/Scripts/NumbersWizard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumbersWizard : MonoBehaviour
+{
+	void Start()
+    {
+		print("Welcome to Numbers Wizard\n" + "Pick a number in your head but don't tell me");
+	}
+}
