08f9f071653a1b2d0d4d3c61b6fa217e1000e449
Bhanu <uabhanu@gmail.com>
2017-07-29 13:08:06 +0100

    Section 2 Lecture 20 Complete

--- NumbersWizard/Assets/Scripts/NumbersWizard.cs
+++ NumbersWizard/Assets/Scripts/NumbersWizard.cs
@@ -8,9 +8,15 @@ public class NumbersWizard : MonoBehaviour
 
 	void Start()
     {
-		print("Welcome to Numbers Wizard\n" + "Pick a number in your head but don't tell me");
-        print("The maximum number you can pick is : " + m_max + "\nThe Lowest number you can pick is : " + m_min); 
-        print("Is the number higher or lower than " + m_guess + "?\n" + "Up arrow for higher, Down arrow for lower & return for equal");
+        m_max++;
+
+        print("Pick a number in your head but don't tell me");
+
+        print("The highest number you can pick is " + m_max);
+        print("The lowest number you can pick is " + m_min);
+
+        print("Is the number higher or lower than : " + m_guess + "?");
+        print("Up = Higher , Down = Lower & Return = Equal");
 	}
 
     void Update()
@@ -24,19 +30,19 @@ public class NumbersWizard : MonoBehaviour
         {
             m_max = m_guess;
             m_guess = (m_max + m_min) / 2;
-            print("Higher or Lower than : " + m_guess + "?\n");
+            print("Higher or Lower than : " + m_guess + "?");
         }
 
         else if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             m_min = m_guess;
             m_guess = (m_max + m_min) / 2;
-            print("Higher or Lower than : " + m_guess + "?\n");
+            print("Higher or Lower than : " + m_guess + "?");
         }
 
         else if(Input.GetKeyDown(KeyCode.Return))
         {
-           
+            print("I Won");
         }
     }
 }
