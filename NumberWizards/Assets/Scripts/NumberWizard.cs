cab1f01cec80577719ddf7439ac4cf371df39bce
Bhanu <uabhanu@gmail.com>
2017-07-29 10:13:44 +0100

    Udemy Game One Number Wizards

---
+++ NumberWizards/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+	void Start()
+    {
+		print("Welcome to Number Wizards\n" + "My Name is Bhanu");
+	}
+}
