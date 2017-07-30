f44119f54d84bbee40a923a6d49b4b8b33f6587b
Bhanu <uabhanu@gmail.com>
2017-07-30 18:01:54 +0100

    Section 4 Lecture 52 Finished

--- NumbersWizard/Assets/Scripts/NumbersWizard.cs
+++ NumbersWizard/Assets/Scripts/NumbersWizard.cs
@@ -1,31 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NumbersWizard : MonoBehaviour
 {
     int m_guess , m_max , m_min;
+    [SerializeField] int m_maxGuessesAllowed;
+    [SerializeField] Text m_guessLabel;
 
 	void Start()
     {
         StartGame();
 	}
 
-    void Update()
+    public void GuessLow()
     {
-        if(Time.timeScale == 0) //Useful for checking if game is paused or not
-        {
-            return;
-        }
+        m_max = m_guess;
+        NextGuess();
+    }
 
-        UserInput();
+    public void GuessHigh()
+    {
+        m_min = m_guess;
+        NextGuess();
     }
 
     void NextGuess()
     {
         m_guess = (m_max + m_min) / 2;
-        print("Higher or Lower than : " + m_guess);
-        print("Up = Higher , Down = Lower & Return = Equal");
+        m_guessLabel.text = m_guess.ToString();
+        m_maxGuessesAllowed--;
+
+        if(m_maxGuessesAllowed < 1)
+        {
+            SceneManager.LoadScene("Win");
+        }
     }
 
     void StartGame()
@@ -33,40 +44,6 @@ public class NumbersWizard : MonoBehaviour
         m_guess = 500;
         m_max = 1000;
         m_min = 1;
-
-        //print("Select a range if numbers between which you want to choose");
-        //print("Please enter maximum number");
-        //m_max should take the user entered number and so on. NumbersWizard Course complete, up to you to finish the rest
-
-        print("Pick a number in your head but don't tell me");
-
-        print("The highest number you can pick is " + m_max);
-        print("The lowest number you can pick is " + m_min);
-
-        print("Is the number higher or lower than : " + m_guess);
-        print("Up = Higher , Down = Lower & Return = Equal");
-
         m_max++;
-    }
-
-    void UserInput()
-    {
-        if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            m_max = m_guess;
-            NextGuess();
-        }
-
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            m_min = m_guess;
-            NextGuess();
-        }
-
-        else if(Input.GetKeyDown(KeyCode.Return))
-        {
-            print("I Won");
-            StartGame();
-        }
     }
 }
